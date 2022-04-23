20cee601758962fbca96318e85fa843c5cccb6c7
Ouu Naja <nextchanupol@gmail.com>
2022-04-23 18:46:25 +0700

    useEffect

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, FormEvent, useState } from "react";
+import { ChangeEvent, FC, FormEvent, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { TransactionSchema } from "../schema/TransactionSchema";
 import "./FormComponent.css";
@@ -12,6 +12,13 @@ const FormComponent: FC<FormComponentProps> = (props) => {
 
 	const [title, setTitle] = useState<string>("");
 	const [amount, setAmount] = useState<number>(0);
+	const [formValid, setFormValid] = useState<boolean>(false);
+
+	useEffect(() => {
+		console.log("useEffect");
+		const isValid: boolean = title.trim().length > 0 && amount !== 0;
+		setFormValid(isValid);
+	}, [title, amount]);
 
 	const inputTitle = (e: ChangeEvent<HTMLInputElement>) => {
 		setTitle(e.target.value);
@@ -22,28 +29,40 @@ const FormComponent: FC<FormComponentProps> = (props) => {
 	};
 	const saveItem = (e: FormEvent<HTMLElement>) => {
 		e.preventDefault();
+		console.log("number", amount);
 		const itemData: TransactionSchema = {
 			id: uuidv4(),
 			title,
-			amount,
+			amount: Number(amount),
 		};
 		onAddNewItem(itemData);
 		setTitle("");
 		setAmount(0);
 	};
+
 	return (
 		<>
 			<form onSubmit={saveItem}>
 				<div className="form-control">
 					<label>รายการ</label>
-					<input type="text" placeholder="รายการ" onChange={inputTitle} />
+					<input
+						type="text"
+						placeholder="รายการ"
+						onChange={inputTitle}
+						value={title}
+					/>
 				</div>
 				<div className="form-control">
 					<label>จำนวนเงิน</label>
-					<input type="number" placeholder="จำนวนเงิน" onChange={inputAmount} />
+					<input
+						type="number"
+						placeholder="จำนวนเงิน"
+						onChange={inputAmount}
+						value={amount}
+					/>
 				</div>
 				<div>
-					<button className="btn" type="submit">
+					<button disabled={!formValid} className="btn" type="submit">
 						Add
 					</button>
 				</div>
